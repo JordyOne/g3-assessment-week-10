3de5a6f281ebf00f7a85332e2ddbbf8c81523bd9
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-08-11 10:39:32 -0600

    added validations to Gif class and it broke app

--- app/models/Gif.rb
+++ app/models/Gif.rb
@@ -1,3 +1,6 @@
 class Gif < ActiveRecord::Base
-
+  validates :title,
+            :presence => {:message => "Title can't be blank"}
+  validates :image_url,
+            :presence => {:message => "Url can't be blank"}
 end
